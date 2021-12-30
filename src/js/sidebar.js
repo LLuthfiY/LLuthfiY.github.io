3bb7112fa308b51c8219b033b1417f7c8d2f24a9
Muhammad Luthfi Albiansyah <46187943+LLuthfiY@users.noreply.github.com>
2021-12-31 03:53:12 +0700

    close

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -7,3 +7,13 @@ function toggleSide() {
     bars.classList.toggle('hidden');
 
 }
+
+function closeSide() {
+    let sidebar = document.getElementById('sidebar');
+    let times= document.getElementById('close');
+    let bars = document.getElementById('bars');
+    sidebar.classList.add('hide-sidebar');
+    times.classList.add('hidden');
+    bars.classList.remove('hidden');
+
+}
