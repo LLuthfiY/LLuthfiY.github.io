335f376e024c6d495e971b0bea4799ce8ec79d94
Muhammad Luthfi Albiansyah <46187943+LLuthfiY@users.noreply.github.com>
2022-10-06 14:51:51 +0700

    lama

--- _app/immutable/chunks/1-dd7baa3e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-cf9dbb03.js";export{t as component};
--- _app/immutable/chunks/2-2ec2ddc5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-f0be11ee.js";export{t as component};
